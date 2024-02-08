8ffb4f9dac5931b755b9b2786f18c2803f761859
waleedahmad4015 <140584021+waleedahmad4015@users.noreply.github.com>
2024-02-08 19:52:39 +0500

    🛠️ index.js -> modified port to use environment variable

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const products_routes = require('./routes/products');
 const connectDB = require('./db/connect');
 
